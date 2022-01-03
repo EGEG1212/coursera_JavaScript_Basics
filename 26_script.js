71b3124013fe6ff5cb2ad357e7d84826cc3780be
egeg1212 <dana1anne@naver.com>
2022-01-04 00:09:06 +0900

    211231

---
+++ 26_script.js
@@ -0,0 +1,23 @@
+(function(){
+    "use strict";
+    const myImages = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"];
+    let currentImage = 0;
+
+    document.getElementById('next').onclick = nextPhoto;
+    document.getElementById('previous').onclick = previousPhoto;
+
+    //myImages.length-1 는 사진 5장인데 인덱스 4까지 만들어줘야하니까.
+    function nextPhoto(){
+        currentImage++;
+        if(currentImage > myImages.length-1){ currentImage = 0;}
+        document.getElementById('myimage').src = myImages[currentImage];
+    }
+
+    function previousPhoto(){
+        currentImage--;
+        
+        if(currentImage < 0){ currentImage =  myImages.length-1;}
+        document.getElementById('myimage').src = myImages[currentImage];
+        
+    }
+})();
